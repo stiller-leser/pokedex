ec0a0d739d5c6ec9cff22c3068996f22ab908d1c
Kaj <kajmossi@yahoo.de>
2014-01-24 10:26:43 +0100

    Added asc test to text_spec

--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -12,7 +12,7 @@ describe "PokedexLists" do
          create(:pokedex_list, name: "Pokemon No #{i}")
       end
     end
-    it "shows the pokemons in the correct order" do
+    it "shows the pokemons in the correct descending order" do
         visit sort_path
         #save_and_open_page
 
@@ -22,6 +22,14 @@ describe "PokedexLists" do
         'Pokemon No 4'.should appear_before('Pokemon No 3')
         'Pokemon No 3'.should appear_before('Pokemon No 2')
         'Pokemon No 2'.should appear_before('Pokemon No 1')
+    end
+
+    it "shows the pokemons in the correct ascending order" do
+        visit sort_path
+        'Pokemon No 1'.should appear_before('Pokemon No 2')
+        'Pokemon No 2'.should appear_before('Pokemon No 3')
+        'Pokemon No 3'.should appear_before('Pokemon No 4')
+        'Pokemon No 4'.should appear_before('Pokemon No 5')
     end
 
     it "count the correct number of pokemon in the index" do
